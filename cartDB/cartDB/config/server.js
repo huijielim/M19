78ce489d43c627692944de16f37d9a05c9cd7126
huijielim <huijielim@gmail.com>
2021-10-24 16:59:25 +0800

    first commit

---
+++ cartDB/cartDB/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f26cc31c946a1a2392606b0d02673f57'),
+    },
+  },
+});
---
+++ cartDB/cartDB/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '55648807-63d7-4b75-aef7-780f6766c13c'
+};
